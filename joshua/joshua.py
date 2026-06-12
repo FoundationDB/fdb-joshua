FoundationDB/fdb-joshua

commit 161c46c23f07279fa9c8240f15ae0eef6c62b230

joshua list: replace spaces and '=' in prop values with '_'
the output was ambiguous durations over a day:

> ... remaining=0:00:00 runtime=2 days, 0:38:14 sanity=False ...

joshua/joshua.py
@@ -24,6 +24,7 @@ import datetime
 import json
 import os
 import pwd
+import re
 import sys
 import threading
 import time
@@ -39,11 +40,12 @@ def get_username():
     return os.environ.get(JOSHUA_USER_ENV, pwd.getpwuid(os.getuid())[0])
 
 
+def str_esc(v):
+    return re.sub(r"[\s=]", "_", str(v))
+
+
 def format_ensemble(e, props):
-    return "  %-50s %s" % (
-        e,
-        " ".join(f"{k}={v}" for k, v in sorted(props.items())),
-    )
+    return f"  {e:50}" + " ".join(f"{k}={str_esc(v)}" for k, v in sorted(props.items()))
 
 
 def timestamp_of(time_string):
@@ -83,11 +85,7 @@ def list_active_ensembles(
         if show_in_progress:
             print("\tCurrently active tests:")
             for props in joshua_model.show_in_progress(e):
-                print(
-                    "\t{}".format(
-                        " ".join(f"{k}={v}" for k, v in sorted(props.items()))
-                    )
-                )
+                print("\t" + " ".join(f"{k}={v}" for k, v in sorted(props.items())))
 
     return ensemble_list
 

commit 8009e2323edee393e22f0b03a8bf98d622cd6d32

joshua list: ensure space between ensemble ID and details
bug introduced in 161c46c23f07 / #145
(and didn't cause problems for manual joshua runs, but in CI
with ensemble ID that hits max length, space was missing)

joshua/joshua.py
@@ -45,7 +45,7 @@ def str_esc(v):
 
 
 def format_ensemble(e, props):
-    return f"  {e:50}" + " ".join(f"{k}={str_esc(v)}" for k, v in sorted(props.items()))
+    return f"  {e:50} " + " ".join(f"{k}={str_esc(v)}" for k, v in sorted(props.items()))
 
 
 def timestamp_of(time_string):