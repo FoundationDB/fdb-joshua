0c61e1ac14c889ec55bd531db6e5e75e22cf990d
Pierce Lopez <pierce.lopez@apple.com>
2026-06-12 16:39:42 -0400

    increase default fdb transaction timeout to 45 seconds
    In a busy joshua cluster, transaction retries and timeouts
    can get pretty bad (joshua seems like not a good use case for fdb)

--- joshua/joshua_model.py
+++ joshua/joshua_model.py
@@ -94,7 +94,7 @@ def open(c_file=None, dir_path=("joshua",)):
     cluster_file = c_file
     db = fdb.open(cluster_file)
     # Timeout applies to all transactions. This should prevent FDB to hang forever.
-    transaction_timeout = int(os.environ.get("TRANSACTION_TIMEOUT_MS", "25000"))
+    transaction_timeout = int(os.environ.get("TRANSACTION_TIMEOUT_MS", "45000"))
     # Retry limit applies to all transactions. This should prevent FDB to hang forever.
     transaction_retry_limit = int(os.environ.get("TRANSACTION_RETRY_LIMIT", "25"))
     logger.info(f"transaction_timeout: {transaction_timeout} and transaction_retry_limit: {transaction_retry_limit}")
