23f0afdb7cbf6e2be4ba854308f3b582a53fc128
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2026-07-07 11:25:14 +0530

    suggested comment added

--- joshua/joshua_agent.py
+++ joshua/joshua_agent.py
@@ -547,6 +547,7 @@ class AsyncEnsemble:
                 pass
 
             # Update the ensemble label
+            # Just in case of ensembles created before joshua_model sanitize id fix
             sanitized_ensemble = joshua_model.sanitize_for_k8s_label(ensemble)
             if pod_desc.metadata.labels.get('ensemble', '') != sanitized_ensemble:
                 v1.patch_namespaced_pod(pod_name, namespace, {"metadata":{"labels":{"ensemble":sanitized_ensemble}}})
